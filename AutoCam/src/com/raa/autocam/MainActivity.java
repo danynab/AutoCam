5030fa44995d116eafab538c7a873f19b082d7bb
Cristian Álvarez Belaustegui <cristian.alvb@gmail.com>
2013-01-18 16:26:09 +0100

    Cambiada la apariencia de la actividad principal.

--- AutoCam/src/com/raa/autocam/MainActivity.java
+++ AutoCam/src/com/raa/autocam/MainActivity.java
@@ -7,22 +7,28 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.raa.autocam.andAr.CustomActivity;
 
 public class MainActivity extends Activity {
+	
+	Integer segundos = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button boton = (Button) findViewById(R.id.button1);
+		Button boton = (Button) findViewById(R.id.MainActivity_boton_comenzar);
 		boton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				segundos = obtenerSegundos();
+				Toast.makeText(MainActivity.this, segundos+"", Toast.LENGTH_SHORT).show();
+				//TODO hacer algo con los segundos
 				Intent i = new Intent(MainActivity.this, CustomActivity.class);
 				startActivity(i);
 			}
@@ -36,4 +42,18 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public Integer obtenerSegundos() {
+		RadioGroup radio = (RadioGroup) findViewById(R.id.MainActivity_radiogroup_segundos);
+		Integer seleccionado = radio.getCheckedRadioButtonId();
+		switch (seleccionado) {
+			case R.id.MainActivity_radio_3:
+				return 3;
+			case R.id.MainActivity_radio_5:
+				return 5;
+			case R.id.MainActivity_radio_10:
+				return 10;
+			default:
+				return null;
+		}
+	}
 }
